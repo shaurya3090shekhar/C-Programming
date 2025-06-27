27446d35fb04558b91a972de3cbe0c42b016202c
Shaurya Shekhar <shaurya.shekhar.cs28@iilm.edu>
2025-06-27 08:30:05 +0530

    1st.c

--- Chapter-0/1st.c
+++ Chapter-0/1st.c
@@ -1,1 +1,17 @@
+/*
+ * File: basic_syntax.c
+ * Description: 
+ *   This is the first C program in the repository. It introduces the basic structure 
+ *   and syntax of a C program, including:
+ *     - Including standard header files
+ *     - Using the main() function
+ *     - Printing output with printf
+ *     - Returning from main
+ * 
+ *   The code demonstrates a simple "Hello, World!" example to get started with C programming.
+ *
+ * Author: [Shaurya Shekhar]
+ * Created on: [27/06/2025]
+ * Repository: https://github.com/shaurya3090shekhar/C-Programming
+ */
 
