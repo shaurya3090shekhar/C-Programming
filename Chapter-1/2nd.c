8db11e8ce778c906951f24016ee927b1a74afe54
Shaurya Shekhar <shaurya.shekhar.cs28@iilm.edu>
2025-06-28 08:43:54 +0530

    2nd.c

--- Chapter-1/2nd.c
+++ Chapter-1/2nd.c
@@ -1,1 +1,19 @@
+/*
+ * File: 2nd.c
+ * Description: 
+ *   This file contains multiple C programs designed to practice and strengthen understanding 
+ *   of basic arithmetic operations and related fundamental programming concepts. It includes:
+ *     - Addition, subtraction, multiplication, division, and modulus operations
+ *     - Use of variables, data types, and expressions
+ *     - Simple problem-solving logic such as swapping numbers, checking even/odd, and divisibility
+ *     - Implementing functions to perform arithmetic operations
+ *     - Input/output handling using scanf and printf
+ * 
+ *   These exercises serve as foundational practice for beginners in C programming, preparing 
+ *   them for more advanced logical and algorithmic challenges.
+ *
+ * Author: [Shaurya Shekhar]
+ * Created on: [28/06/2025]
+ * Repository: https://github.com/shaurya3090shekhar/C-Programming
+ */
 
