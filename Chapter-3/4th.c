a026ad0c3d611e12ace568b55ce3b5329066b473
Shaurya Shekhar <shaurya.shekhar.cs28@iilm.edu>
2025-07-03 21:27:33 +0530

    4th.c

--- Chapter-3/4th.c
+++ Chapter-3/4th.c
@@ -1,1 +1,19 @@
+/*
+ * File: functions_practice.c
+ * Description: 
+ *   This file contains a collection of C programs centered around the concept of functions. 
+ *   It is aimed at helping learners develop a deep understanding of modular programming 
+ *   and reusability in C. Key concepts covered include:
+ *     - Function declaration, definition, and calling conventions
+ *     - Parameter passing (by value and by reference)
+ *     - Return types, including void and various data types
+ *     - Best practices for organizing and structuring code using functions
+ *
+ *   These examples serve as practical exercises for mastering user-defined functions, improving
+ *   code readability, and preparing for real-world programming scenarios.
+ *
+ * Author: [Shaurya Shekhar]
+ * Created on: [03/07/2025]
+ * Repository: https://github.com/shaurya3090shekhar/C-Programming
+ */
 
